8c14dc439b276fdbe9f074967dd4e399f09e2e78
yaohaofei <313888178@qq.com>
2024-11-27 17:21:15 +0800

    6.3.4.6

--- OSETSDK/OSETSDK.framework/Headers/OSETSDK.h
+++ OSETSDK/OSETSDK.framework/Headers/OSETSDK.h
@@ -17,25 +17,4 @@ FOUNDATION_EXPORT const unsigned char OSETSDKVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <OSETSDK/PublicHeader.h>
 
 #import <OSETSDK/OSETManager.h>
-#import <OSETSDK/OSETBannerAd.h>
-#import <OSETSDK/OSETSplashAd.h>
-#import <OSETSDK/OSETInterstitialAd.h>
-#import <OSETSDK/OSETInterstitialLandscapeAd.h>
-#import <OSETSDK/OSETRewardVideoAd.h>
-#import <OSETSDK/OSETFullscreenVideoAd.h>
-#import <OSETSDK/OSETNativeAd.h>
-#import <OSETSDK/OSETDrawAd.h>
-#import <OSETSDK/OSETConsult.h>
-#import <OSETSDK/OSETYDConsult.h>
-#import <OSETSDK/OSETConstellatoryAd.h>
-#import <OSETSDK/OSETDialAd.h>
-#import <OSETSDK/OSETDrawView.h>
-#import <OSETSDK/OSETLootTreasureAd.h>
-#import <OSETSDK/OSETWebDialAd.h>
-#import <OSETSDK/OSETCalendarAd.h>
-#import <OSETSDK/OSETCrazyDreamAd.h>
-#import <OSETSDK/OSETSuspendAd.h>
-#import <OSETSDK/OSETWeatherAd.h>
-#import <OSETSDK/OSETAnswerAd.h>
-#import <OSETSDK/OSETCorrespondenceAd.h>
-#import <OSETSDK/OSETBaseView.h>
+
